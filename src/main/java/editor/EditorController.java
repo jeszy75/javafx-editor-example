75d80fa6c2d0dadd4be0b2811bdbbbf77219935d
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2026-05-01 23:06:29 +0200

    refactor: remove unused import

--- src/main/java/editor/EditorController.java
+++ src/main/java/editor/EditorController.java
@@ -2,7 +2,6 @@ package editor;
 
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
